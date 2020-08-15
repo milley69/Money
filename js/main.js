720e17e8edf597501566197f473706357b3e749f
milley69 <69milley@gmail.com>
2020-08-15 20:17:51 +0500

    delet all

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  $('.header__burger').click(function(event) {
-    $('nav,.header__burger,.login').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-});
-
-// if (window.screen.width >= 768) {
-//   document.querySelector('.null').style.display = "none"
-// } else {
-//   document.querySelector('.null').style.display = "block"
-// }
+// $(document).ready(function() {
+//   $('.header__burger').click(function(event) {
+//     $('nav,.header__burger,.login').toggleClass('active');
+//     $('body').toggleClass('lock');
+//   });
+// });
+//
+// // if (window.screen.width >= 768) {
+// //   document.querySelector('.null').style.display = "none"
+// // } else {
+// //   document.querySelector('.null').style.display = "block"
+// // }
